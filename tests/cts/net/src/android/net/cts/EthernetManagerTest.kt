dab1e833dee836f81ecbd8302943a100aa8dd841
Liana Kazanova (xWF) <lkazanova@google.com>
2025-07-17 11:17:59 -0700

    Revert "Fix EthernetManagerTest"
    This reverts commit e9dcc62d2da65516ec319fd89ba613211930d482.
    
    Reason for revert:  DroidMonitor: reverting due to http://b/432497516. I will verify through ABTD before revert submission. 
    
    https://buganizer.corp.google.com/issues/432497516#comment6
    
    Bug: 432497516
    Change-Id: I4773c0493137367cb0d4167eab535e0d827956ee

--- tests/cts/net/src/android/net/cts/EthernetManagerTest.kt
+++ tests/cts/net/src/android/net/cts/EthernetManagerTest.kt
@@ -964,7 +964,6 @@ class EthernetManagerTest {
 
     @Test
     fun testEnableDisableInterface_disableEnableEthernet() {
-        assumeFalse(restoreEthernetRule.isAdbOverEthernet())
         val iface = createInterface()
         val listener = EthernetStateListener()
         addInterfaceStateListener(listener)
@@ -976,6 +975,7 @@ class EthernetManagerTest {
         listener.eventuallyExpect(iface, STATE_LINK_DOWN, ROLE_CLIENT)
         enableInterface(iface).expectError()
         disableInterface(iface).expectError()
+        listener.assertNoCallback()
 
         // When ethernet is enabled, enable/disableInterface() should succeed.
         setEthernetEnabled(true)
@@ -1127,8 +1127,6 @@ class EthernetManagerTest {
 
     @Test
     fun testRemoveInterface_disableEnableEthernet() {
-        assumeNoInterfaceForTetheringAvailable()
-
         // Set up 2 interfaces for testing
         val iface1 = createInterface()
         val listener = EthernetStateListener()
