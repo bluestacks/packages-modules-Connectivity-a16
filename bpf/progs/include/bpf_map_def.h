8f6d2d86d5405b0b3fd914b4c31cc707d7bbcdd4
Maciej Żenczykowski <maze@google.com>
2025-07-30 17:57:25 -0700

    bpf_map_def: rename BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE to BPF_DEF_CHAR_ARRAY_SIZE
    as the name is no longer really appropriate and I want to add another use...
    
    Generated via:
      sed -i 's@BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE@BPF_DEF_CHAR_ARRAY_SIZE@' bpf/progs/include/bpf_map_def.h
    
    Test: TreeHugger
    Flag: EXEMPT mainline
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I972f8cababe79070aa62f728733c56f41e7a7a84

--- bpf/progs/include/bpf_map_def.h
+++ bpf/progs/include/bpf_map_def.h
@@ -105,7 +105,7 @@ struct optional_bool { bool optional; };
 
 // Length of strings (incl. selinux_context and pin_subdir)
 // in the bpf_map_def and bpf_prog_def structs.
-#define BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE 66  // must be even for alignment sanity
+#define BPF_DEF_CHAR_ARRAY_SIZE 66  // must be even for alignment sanity
 
 /*
  * Map structure to be used by Android eBPF C programs. The Android eBPF loader
@@ -142,13 +142,13 @@ struct bpf_map_def {
     unsigned int max_kver;
 
     // These are fixed length ASCIIZ strings, padded with null bytes
-    char create_location[BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE];
-    char pin_prefix[BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE];
+    char create_location[BPF_DEF_CHAR_ARRAY_SIZE];
+    char pin_prefix[BPF_DEF_CHAR_ARRAY_SIZE];
 };
 
 // This needs to be updated whenever the above structure definition is expanded.
 // These asserts are here to make sure we have cross-6-arch consistency.
-_Static_assert(sizeof(struct bpf_map_def) == 48 + 2 * BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE, "wrong sizeof struct bpf_map_def");
+_Static_assert(sizeof(struct bpf_map_def) == 48 + 2 * BPF_DEF_CHAR_ARRAY_SIZE, "wrong sizeof struct bpf_map_def");
 _Static_assert(__alignof__(struct bpf_map_def) == 4, "__alignof__ struct bpf_map_def != 4");
 _Static_assert(_Alignof(struct bpf_map_def) == 4, "_Alignof struct bpf_map_def != 4");
 
@@ -167,12 +167,12 @@ struct bpf_prog_def {
     unsigned int bpfloader_min_ver;
     unsigned int bpfloader_max_ver;
 
-    char create_location[BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE];
-    char pin_prefix[BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE];
+    char create_location[BPF_DEF_CHAR_ARRAY_SIZE];
+    char pin_prefix[BPF_DEF_CHAR_ARRAY_SIZE];
 };
 
 // This needs to be updated whenever the above structure definition is expanded.
 // These asserts are here to make sure we have cross-6-arch consistency.
-_Static_assert(sizeof(struct bpf_prog_def) == 28 + 2 * BPF_PIN_SUBDIR_CHAR_ARRAY_SIZE, "wrong sizeof struct bpf_prog_def");
+_Static_assert(sizeof(struct bpf_prog_def) == 28 + 2 * BPF_DEF_CHAR_ARRAY_SIZE, "wrong sizeof struct bpf_prog_def");
 _Static_assert(__alignof__(struct bpf_prog_def) == 4, "__alignof__ struct bpf_prog_def != 4");
 _Static_assert(_Alignof(struct bpf_prog_def) == 4, "_Alignof struct bpf_prog_def != 4");
