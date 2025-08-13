a3ca73e8209d25365d280795a2223e29764c1a77
Patrick Rohr <prohr@google.com>
2025-08-12 21:13:10 -0700

    Revert "Ignore null or empty interface names in EthernetTracker."
    This reverts commit 28961f74648dfd0f5ecc7baadce0728ab4fc1153.
    
    Reason for revert: ag/34381937 fixed this one layer up.
    
    Change-Id: I07f0080d137ec59375d001efb6183af108f87dfb

--- service-t/src/com/android/server/ethernet/EthernetTracker.java
+++ service-t/src/com/android/server/ethernet/EthernetTracker.java
@@ -287,8 +287,6 @@ public class EthernetTracker {
             // (for existing interfaces). Up until then, this code runs for every network interface
             // on the system.
             final String ifname = msg.getInterfaceName();
-            if (TextUtils.isEmpty(ifname)) return;  // TODO: can this happen?
-
             final EthernetPort port = new EthernetPort(ifname, mac, ifinfomsg.index);
 
             switch (msg.getHeader().nlmsg_type) {
