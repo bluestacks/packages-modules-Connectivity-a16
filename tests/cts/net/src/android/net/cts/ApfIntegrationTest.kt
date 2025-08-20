392abd127f81d6f9dfcbe96659fbf06c96a42032
Antonio Kantek <kanant@google.com>
2025-08-19 21:15:03 -0700

    Remove automotive MUMD workaround in ApfIntegrationTest.
    As per `Iad43a122407fe438cab94f59ed8aba8d6080af02` commit, this test is no longer running on automotive.
    
    This changelist also skips this test on Android Automotive within the setupOnce (@BeforeClass) method to avoid
    executing unnecessary SLEEP/WAKEUP logic.
    
    Fix: 439953326
    Test: atest CtsNetTestCases:ApfIntegrationTest
    Test: presubmit
    Flag: TEST_ONLY
    Change-Id: Ic96db7a5dbc3f793bef85d9d3cd10cbcf726c7b4

--- tests/cts/net/src/android/net/cts/ApfIntegrationTest.kt
+++ tests/cts/net/src/android/net/cts/ApfIntegrationTest.kt
@@ -54,7 +54,6 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.os.PowerManager
 import android.os.SystemProperties
-import android.os.UserManager
 import android.platform.test.annotations.AppModeFull
 import android.system.Os
 import android.system.OsConstants
@@ -136,6 +135,7 @@ class ApfIntegrationTest {
 
         private val context = InstrumentationRegistry.getInstrumentation().context
         private val powerManager = context.getSystemService(PowerManager::class.java)!!
+        private val pm = context.packageManager
         private val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
         private var isLowPowerStandbyOriginalEnabled: Boolean = false
         private var originalPolicy: FromU<PowerManager.LowPowerStandbyPolicy?>? = null
@@ -153,29 +153,10 @@ class ApfIntegrationTest {
         }
 
         private fun waitForInteractiveState(interactive: Boolean) {
-            // TODO(b/366037029): This test condition should be removed once
-            // PowerManager#isInteractive is fully implemented on automotive
-            // form factor with visible background user.
-            if (isAutomotiveWithVisibleBackgroundUser()) {
-                // Wait for 2 seconds to ensure the interactive state is updated.
-                // This is a workaround for b/366037029.
-                Thread.sleep(2000L)
-            } else {
-                val result = pollingCheck(timeout_ms = 2000) {
-                    powerManager.isInteractive()
-                }
-                assertThat(result).isEqualTo(interactive)
+            val result = pollingCheck(timeout_ms = 2000) {
+                powerManager.isInteractive()
             }
-        }
-
-        private fun isAutomotiveWithVisibleBackgroundUser(): Boolean {
-            val packageManager = context.getPackageManager()
-            val userManager = context.getSystemService(UserManager::class.java)!!
-            return (packageManager.hasSystemFeature(FEATURE_AUTOMOTIVE) &&
-                    // isVisibleBackgroundUsersSupported is @TestApi, but this test should build
-                    // against module API stubs, which do not include it (b/409931932).
-                    userManager.javaClass.getMethod("isVisibleBackgroundUsersSupported")
-                        .invoke(userManager) as Boolean)
+            assertThat(result).isEqualTo(interactive)
         }
 
         private fun disableLowPowerStandby() {
@@ -211,6 +192,14 @@ class ApfIntegrationTest {
         fun setupOnce() {
             // TODO: assertions thrown in @BeforeClass / @AfterClass are not well supported in the
             // test infrastructure. Consider saving exception and throwing it in setUp().
+
+            if (pm.hasSystemFeature(FEATURE_AUTOMOTIVE)) {
+                // Skip on Android Automotive to avoid running unnecessary SLEEP/WAKEUP logic.
+                // Ideally, this would use assumeFalse(isAutomotive) here, but this isn't fully
+                // supported by the test infra (see comment above). Thus, the proper assumption
+                // check is later done in the #setup (@Before).
+                return
+            }
 
             // APF must run when the screen is off and the device is not interactive.
             turnScreenOff()
@@ -318,7 +307,6 @@ class ApfIntegrationTest {
     @get:Rule val expect = Expect.create()
 
     private val cm by lazy { context.getSystemService(ConnectivityManager::class.java)!! }
-    private val pm by lazy { context.packageManager }
     private lateinit var network: Network
     private lateinit var ifname: String
     private lateinit var networkCallback: TestableNetworkCallback
